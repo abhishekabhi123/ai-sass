a4e4891a79e9746a0d1fb3253f7aa9db7fc08ad9
abhishekabhi123 <vadakkemadamabhishek@gmail.com>
2025-08-18 23:47:20 +0530

    added supabase

---
+++ lib/actions/companion.actions.ts
@@ -0,0 +1,15 @@
+import { auth } from "@clerk/nextjs/server";
+import { createSupabaseClient } from "../supabase";
+
+export const createCompanion = async (FormData: CreateCompanion) => {
+  const { userId: author } = await auth();
+  const supabase = createSupabaseClient();
+
+  const { data, error } = await supabase.from(relations: 'Companions').insert({ ...FormData, author }).select();
+
+  if (error || !data) {
+    throw new Error(error?.message || 'Failed to create a companion')
+  }
+
+  return data[0];
+};
