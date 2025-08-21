028f80954e15cecf447ba543246603729cb69107
abhishekabhi123 <vadakkemadamabhishek@gmail.com>
2025-08-21 20:08:50 +0530

    added ui for specific compnion

--- lib/actions/companion.actions.ts
+++ lib/actions/companion.actions.ts
@@ -42,3 +42,14 @@ export const getAllCompanions = async ({
   if (error) throw new Error(error.message);
   return companions;
 };
+
+export const getCompanion = async (id: string) => {
+  const supabase = createSupabaseClient();
+  const { data, error } = await supabase
+    .from("Companions")
+    .select()
+    .eq("id", id);
+  if (error) return console.log(error);
+
+  return data[0];
+};
